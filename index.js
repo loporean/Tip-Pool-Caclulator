7c5feee77aee14822b049c2b0c6244f2b7437f21
loporean <84533280+loporean@users.noreply.github.com>
2024-07-10 11:43:02 -0700

    Organizing

--- index.js
+++ index.js
@@ -10,25 +10,27 @@
         // Total tips
         var totTips = 0;
 
-        // Grab the count from local storage
-        var count=localStorage.getItem("count", count);
-        console.log(count);
+        // // Grab the count from local storage
+        // var count=localStorage.getItem("count", count);
+        // console.log(count);
 
-        // If count is null, initialize to 1 for initial row
-        if (typeof count === 'undefined' || count == null || isNaN(count)) {
-            count = 1;
-            console.log("count set to", count);
-            // Save to local storage
-            localStorage.setItem("count", count);
-        }
+        // // If count is null, initialize to 1 for initial row
+        // if (typeof count === 'undefined' || count == null || isNaN(count)) {
+        //     count = 1;
+        //     console.log("count set to", count);
+        //     // Save to local storage
+        //     localStorage.setItem("count", count);
+        // }
+
+        var count = 1;
 
         // Adds another employee field for calculating
         function addEmployee() {
             // incrememnt employee count
             count++;
 
-            // Save to local storage
-            localStorage.setItem("count", count);
+            // // Save to local storage
+            // localStorage.setItem("count", count);
 
             console.log("Add employee " + count);
 
@@ -74,8 +76,8 @@
                     container.removeChild(container.lastChild);
                 }
                 count--;
-                // Save to local storage
-                localStorage.setItem("count", count);
+                // // Save to local storage
+                // localStorage.setItem("count", count);
             }
         }
 
@@ -182,14 +184,16 @@
 
         // If local storage contains previous count
         // then generate fields from previous session
-        function restoreFields() {
-            for (let i=0; i<count-1; i++) {
-                addEmployee();
-            }
-        }
+        // function restoreFields() {
+        //     for (let i=0; i<count-1; i++) {
+        //         addEmployee();
+        //     }
+        // }
 
-        if (count != 1) {
-            restoreFields();
-        }
+        // Creating an infinite loop
+        // window.onload = function() {
+        //     restoreFields();
+        // };
 
-        // window.onload = resetFormFields;
+
+        window.onload = resetFormFields;
